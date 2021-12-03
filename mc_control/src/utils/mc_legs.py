09100e4e252e02c091581e1d82d21f07bd54300d
elmer.escandon <elmer.escandon@utec.edu.pe>
2021-12-03 13:13:08 -0500

    Modified FK

--- mc_control/src/utils/mc_legs.py
+++ mc_control/src/utils/mc_legs.py
@@ -2,121 +2,140 @@
 from fr_utils import *
 import numpy as np
 
+# ==========================
+# Constants for the robot
+# ==========================
+
+# Obtain from DH parameters
+A = 0.077476 # Thigh offset * -1 
+B = 0.2115
+C = 0.2309
+
+# Transform from base to toe
+leg_offset_x = 0.196
+leg_offset_y = 0.049664
+
+# =================================
+# Forward Kinematics for each leg
+# ================================
+
 # Front Right Leg from base
-def fk_FR(q,type = 'tran'):
-    q1 = q[0]
-    q2 = q[1]
-    q3 = q[2]
-
-    # Obtain from DH parameters
-    A = 0.077476 # Thigh offset * -1 
-    B = 0.2115
-    C = 0.2309
-
-    T01 = dh(q1 + np.pi, 0, 0, np.pi/2)
-    T12 = dh(q2 + np.pi, -A, B, 0)
-    T23 = dh(q3, 0, C, 0)
-    T03 = T01.dot(T12).dot(T23)
-
-    # Transform from base to toe
-    leg_offset_x = 0.196
-    leg_offset_y = -0.049664
-    T_B = sTrasl(leg_offset_x, leg_offset_y, 0)
-    T_R = sTroty(np.pi/2)
-    TBR_0 = T_B.dot(T_R).dot(T03)
+def fk_FR(q,type = 'tran',joint = 3,group=False):
+    """
+    Returns the Transformation Matrix from the robot base to the Front Right Joints
+    According to the 'joint' variale, the joint is selected. If the 'group' variable is True 
+    (false by default) it returns the accumulated array of transformation matrix
+    """
     
+    # Obtain from DH parameters
+    TFR_01 = dh(q[0] + np.pi, 0, 0, np.pi/2)
+    TFR_12 = dh(q[1] + np.pi, -A, B, 0)
+    TFR_23 = dh(q[2], 0, C, 0)
 
-    if type == 'pos': 
-        return TBR_0[0:3,3]
-    elif type == 'tran':
-        return TBR_0
+    TFR_B0 = sTrasl(leg_offset_x, -leg_offset_y, 0).dot(sTroty(np.pi/2))   
+    TFR_B1 = TFR_B0.dot(TFR_01)
+    TFR_B2 = TFR_B0.dot(TFR_01).dot(TFR_12)
+    TFR_B3 = TFR_B0.dot(TFR_01).dot(TFR_12).dot(TFR_23)
+
+    T = [TFR_B0,TFR_B1,TFR_B2,TFR_B3]
+
+    if group and type == 'tran':
+        return T[0:joint+1]
+    else:
+        if type == 'pos': 
+            T_r = T[joint]
+            return T_r[0:3,3]
+        elif type == 'tran':
+            return T[joint]
 
     
 # Front Left Leg from base
-def fk_FL(q,type = 'tran'):
-    q1 = q[0]
-    q2 = q[1]
-    q3 = q[2]
+def fk_FL(q,type = 'tran',joint = 3,group=False):
+    """
+    Returns the Transformation Matrix from the robot base to the Front Left Joints
+    According to the 'joint' variale, the joint is selected. If the 'group' variable is True 
+    (false by default) it returns the accumulated array of transformation matrix
+    """
 
-    # Obtain from DH parameters
-    A = 0.077476 # Thigh offset * -1 
-    B = 0.2115
-    C = 0.2309
+    TFL_01 = dh(q[0] + np.pi, 0, 0, np.pi/2)
+    TFL_12 = dh(q[1] + np.pi, A, B, 0)
+    TFL_23 = dh(q[2], 0, C, 0)
 
-    T01 = dh(q1 + np.pi, 0, 0, np.pi/2)
-    T12 = dh(q2 + np.pi, A, B, 0)
-    T23 = dh(q3, 0, C, 0)
-    T03 = T01.dot(T12).dot(T23)
-
-    # Transform from base to toe
-    leg_offset_x = 0.196
-    leg_offset_y = 0.049664
-    T_B = sTrasl(leg_offset_x, leg_offset_y, 0)
-    T_R = sTroty(np.pi/2)
-    TBR_0 = T_B.dot(T_R).dot(T03)
+    TFL_B0 = sTrasl(leg_offset_x, leg_offset_y, 0).dot(sTroty(np.pi/2))
+    TFL_B1 = TFL_B0.dot(TFL_01)
+    TFL_B2 = TFL_B0.dot(TFL_01).dot(TFL_12)
+    TFL_B3 = TFL_B0.dot(TFL_01).dot(TFL_12).dot(TFL_23)
     
+    T = [TFL_B0,TFL_B1,TFL_B2,TFL_B3]
 
-    if type == 'pos': 
-        return TBR_0[0:3,3]
-    elif type == 'tran':
-        return TBR_0
+    if group and type=='tran':
+        return T[0:joint+1]
+    else:        
+        if type == 'pos': 
+            T_r = T[joint]
+            return T_r[0:3,3]
+        elif type == 'tran':
+            return T[joint]
 
 
 # Rear Right Leg from base
-def fk_RR(q,type = 'tran'):
-    q1 = q[0]
-    q2 = q[1]
-    q3 = q[2]
+def fk_RR(q,type = 'tran',joint = 3,group=False):
+    """
+    Returns the Transformation Matrix from the robot base to the Rear Right Joints
+    According to the 'joint' variale, the joint is selected. If the 'group' variable is True 
+    (false by default) it returns the accumulated array of transformation matrix
+    """
 
-    # Obtain from DH parameters
-    A = 0.077476 # Thigh offset * -1 
-    B = 0.2115
-    C = 0.2309
+    TRR_01 = dh(q[0] + np.pi, 0, 0, np.pi/2)
+    TRR_12 = dh(q[1] + np.pi, -A, B, 0)
+    TRR_23 = dh(q[2], 0, C, 0)
 
-    T01 = dh(q1 + np.pi, 0, 0, np.pi/2)
-    T12 = dh(q2 + np.pi, -A, B, 0)
-    T23 = dh(q3, 0, C, 0)
-    T03 = T01.dot(T12).dot(T23)
+    TRR_B0 = sTrasl(-leg_offset_x,-leg_offset_y, 0).dot(sTroty(np.pi/2))
+    TRR_B1 = TRR_B0.dot(TRR_01)
+    TRR_B2 = TRR_B0.dot(TRR_01).dot(TRR_12)
+    TRR_B3 = TRR_B0.dot(TRR_01).dot(TRR_12).dot(TRR_23)
+ 
+    T = [TRR_B0,TRR_B1,TRR_B2,TRR_B3]
 
-    # Transform from base to toe
-    leg_offset_x = -0.196
-    leg_offset_y = -0.049664
-    T_B = sTrasl(leg_offset_x, leg_offset_y, 0)
-    T_R = sTroty(np.pi/2)
-    TBR_0 = T_B.dot(T_R).dot(T03)
-    
-
-    if type == 'pos': 
-        return TBR_0[0:3,3]
-    elif type == 'tran':
-        return TBR_0
+    if group and type=='tran':
+        return T[0:joint+1]
+    else: 
+        if type == 'pos':
+            T_r = T[joint]
+            return T_r[0:3,3]
+        elif type == 'tran':
+            return T[joint]
 
 
 # Rear Left Leg from base
-def fk_RL(q,type = 'tran'):
-    q1 = q[0]
-    q2 = q[1]
-    q3 = q[2]
+def fk_RL(q,type = 'tran',joint = 3, group=False):
+    """
+    Returns the Transformation Matrix from the robot base to the Rear Left Joints
+    According to the 'joint' variale, the joint is selected. If the 'group' variable is True 
+    (false by default) it returns the accumulated array of transformation matrix
+    """
 
-    # Obtain from DH parameters
-    A = 0.077476 # Thigh offset * -1 
-    B = 0.2115
-    C = 0.2309
+    TRL_01 = dh(q[0] + np.pi, 0, 0, np.pi/2)
+    TRL_12 = dh(q[1] + np.pi, A, B, 0)
+    TRL_23 = dh(q[2], 0, C, 0)
 
-    T01 = dh(q1 + np.pi, 0, 0, np.pi/2)
-    T12 = dh(q2 + np.pi, A, B, 0)
-    T23 = dh(q3, 0, C, 0)
-    T03 = T01.dot(T12).dot(T23)
+    TRL_B0 = sTrasl(-leg_offset_x, leg_offset_y, 0).dot(sTroty(np.pi/2))
+    TRL_B1 = TRL_B0.dot(TRL_01)
+    TRL_B2 = TRL_B0.dot(TRL_01).dot(TRL_12)
+    TRL_B3 = TRL_B0.dot(TRL_01).dot(TRL_12).dot(TRL_23)
 
-    # Transform from base to toe
-    leg_offset_x = -0.196
-    leg_offset_y = 0.049664
-    T_B = sTrasl(leg_offset_x, leg_offset_y, 0)
-    T_R = sTroty(np.pi/2)
-    TBR_0 = T_B.dot(T_R).dot(T03)
-    
+    T = [TRL_B0,TRL_B1,TRL_B2,TRL_B3]
 
-    if type == 'pos': 
-        return TBR_0[0:3,3]
-    elif type == 'tran':
-        return TBR_0
+    if group and type == 'tran': 
+        return T[0:joint+1]
+    else:
+        if type == 'pos': 
+            T_r = T[joint]
+            return T_r[0:3,3]
+        elif type == 'tran':
+            return T[joint]
+
+
+# =================================
+# Jacobian calculations
+# ================================
--- mc_control/src/utils/test_v1.py
+++ mc_control/src/utils/test_v1.py
@@ -3,6 +3,7 @@ import numpy as np
 from mc_legs import *
 
 
-q = np.array([0,0,0])
-T = fk_FR(q)
-print(np.round(T,2))
+q = np.array([np.pi/3 ,0,0])
+
+T = fk_RL(q,joint=3,group=True)
+print(np.round(T,5))
